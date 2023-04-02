81d6e3e76425d02260351da3bd82b0355990050f
Simone Melidoni <simone.melidoni@gmail.com>
2023-04-02 19:06:00 +0100

    Updated project to be setup with TS and SCSS (#2)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import React, { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className="app">
+      <header className="app-header">React Calculator App</header>
+    </div>
+  );
+};
+
+export default App;
