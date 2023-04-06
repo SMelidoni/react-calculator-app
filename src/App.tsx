dbd1cb3ea134a2fefbcc962e7733340c0d1f23ea
SMelidoni <simone.melidoni@gmail.com>
2023-04-06 13:09:57 +0100

    Updated App.tsx to include back button prop

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import './App.scss';
 import React, { FC } from 'react';
 import CalculatorHome from './pages/calculator-home/calculator-home.component';
 
-const App: FC = () => {
+interface AppProps {
+  showBackButton?: boolean;
+}
+
+const App: FC<AppProps> = ({ showBackButton }) => {
   return (
     <div className="global-page-container">
-      <CalculatorHome />
+      <CalculatorHome showBackButton={showBackButton} />
     </div>
   );
 };
